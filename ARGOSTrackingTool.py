c3d3ac31bca9ecb5c1561c7f025e4f694adf17a2
allybarbaro <allisonbarbaro@gmail.com>
2023-10-04 12:31:55 -0400

    allow user to set date and show location

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -8,6 +8,8 @@
 # Author: John Fay (john.fay@duke.edu)
 # Date:   Fall 2023
 #--------------------------------------------------------------
+#Ask user for a date
+user_date = input("Enter a date: ")
 
 #Create a variable pointing to the data file
 file_name = './data/raw/sara.txt'
@@ -48,3 +50,18 @@ for lineString in line_list:
         location_dict[record_id] = (obs_lat, obs_lon)
     #Print the location of sara
     #print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
+
+#Initialize key list
+keys = []
+
+#Loop through items in date_dict
+for key, value in date_dict.items():
+    if value == user_date: 
+        keys.append(key)
+
+#Loop through keys and report locations
+for key in keys:
+    location = location_dict[key]
+    lat = location[0]
+    long = location[1]
+    print(f"On {user_date}, Sara the turtle was seen at {lat}d Lat, {long}d Lng.")
